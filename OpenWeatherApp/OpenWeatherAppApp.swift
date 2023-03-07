b007edf519c034b8c5328909be7817f2f9cd33f5
Mostafa Elshazly <mostafa.elshazly@anoda.mobi>
2023-03-07 20:00:48 +0200

    Initial Commit

---
+++ OpenWeatherApp/OpenWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherAppApp.swift
+//  OpenWeatherApp
+//
+//  Created by Mostafa Elshazly on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
